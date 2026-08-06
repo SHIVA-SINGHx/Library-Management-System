5a7bbb5952a8e96659e7199e07bc4caed3975b42
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-08-06 18:52:00 +0530

    created SendOtp function

---
+++ backend/utils/sendOtp.js
@@ -0,0 +1,20 @@
+import { createTransport } from "nodemailer";
+
+const sendOtp = async (email, otp)=>{
+    const transport = createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    await transport.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "Your OTP Code",
+        html: <h2>This is your 6-digit {otp}</h2>
+    });
+}
+
+export default sendOtp
