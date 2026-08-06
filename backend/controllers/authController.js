f8054a8e15e612cc0b43f383eedcb783732b5ffd
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-08-06 22:47:22 +0530

    created userLogin function in authController

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,10 @@ import { generate } from "otp-generator";
 import User from "../models/UserModel.js";
 import sendOtp from "../utils/sendOtp.js";
 import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken"
 
+
+// register user
 export async function registerUser(req, res) {
   try {
     const { email, password, name, phoneNo } = req.body;
@@ -107,17 +110,17 @@ export async function completeProfile(req, res) {
 
     const { email, department, semester, year, rollNo, stream } = req.body;
     if (!email)
-      return res.status(400).json({
+      return res.status(404).json({
         message: "Email not found",
       });
 
     const user = await User.findOne({ email });
     if (!user)
-      return res.status(400).json({
+      return res.status(404).json({
         message: "User not found",
       });
     if (!user.isVerified)
-      return res.status(400).json({
+      return res.status(403).json({
         message: "User not verified",
       });
 
@@ -135,10 +138,63 @@ export async function completeProfile(req, res) {
 
   } catch (error) {
     console.log(error)
-    return response.status(400).json({
+    return response.status(500).json({
         message: 'Error completing profile',
         error: error.message
     })
 
   }
+}
+
+// login as user or admin
+export async function loginUser(req, res){
+    try {
+        const {email, password} = req.body;
+        if(!email || password){
+            return res.status(400).json({
+                success: false,
+                message: 'Email and Password are required'
+            })
+        }
+
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        })
+    }
+
+    if(!user.isVerified){
+        return res.status(403).json({
+            success: false,
+            message: "Please verify your email with OTP before login in"
+        })
+    }
+
+    if(!(await bcrypt.compare(password, user.password))){
+        return res.status(400).json({
+            success: false,
+            message: "Invalid credentials"
+        })
+    }
+
+    const token = jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET, {expiresIn: "7d"})
+    const {password, _, ...userResponse} = user.toObject();
+
+    res.status(200).json({
+        success: true,
+        token,
+        user: userResponse
+    })
+        
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            message: "Login user failed "
+        })
+    }
+
+
+
 }
