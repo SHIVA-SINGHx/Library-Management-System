6152ea185a7dd74fbc381e3ea985650477d5e688
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-08-07 18:54:29 +0530

    created authToken func in authMiddleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import User from "../models/UserModel.js";
+
+
+export const authToken = async(req, res, next)=>{
+    try {
+        const authHeaders = req.headers("authorization")
+        const token = authHeaders && authHeaders.split(" ")[1];
+
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: 'No token provided, authorization denied'
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id).select("-password");
+
+        if (!user){
+            return res.status(401).json({
+                success :false,
+                message: 'Token is not valid and user is no longer exist'
+            })
+        }
+
+    } catch (error) {
+        console.error("JWT auth error:", error);
+        res.status(500).json({ message: "token is not valid", error: error.message });
+    }
+}
