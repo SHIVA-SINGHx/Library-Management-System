0f86800aa6e601e4194095a5b2d19dab49126069
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-09-04 12:53:23 +0530

    changed the url

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://http://3.91.22.87";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://https://library-management-system-hj10.onrender.com";
 
 export const API_AUTH_URL = `${API_BASE_URL}/api/auth`;
 export const API_BOOKS_URL = `${API_BASE_URL}/api/books`;
